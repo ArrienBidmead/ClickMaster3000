411704eb209d6428ad7c8f02e9344d349a21653d
GametraiN1 <37019464+GametraiN1@users.noreply.github.com>
2021-03-01 15:04:19 +0000

    Organize into CM3000v2 class

--- CM3000v2/Source/Source.cpp
+++ CM3000v2/Source/Source.cpp
@@ -5,31 +5,17 @@
 #include <Windows.h>
 #include <Dwmapi.h>
 
+#include "Source.h"
+
 #pragma comment (lib, "Dwmapi.lib")
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")		// Disable console
 
-int main()
+void CM3000v2::Init()
 {
-    sf::RenderWindow window(sf::VideoMode(208, 80), "ClickMaster 3000", sf::Style::None);
-
-    MARGINS margins;
-    margins.cxLeftWidth = -1;
-    HWND hwnd = window.getSystemHandle();
-
-    SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-    DwmExtendFrameIntoClientArea(hwnd, &margins);
-
-    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);		//locks app in front of everything else
-
-    //////////////////////////////////////////////////////////////////////////
-
     Textures textures;
 
-    sf::Texture overlapTex;
     overlapTex.loadFromMemory(textures.ButtonOverlap1png, sizeof(textures.ButtonOverlap1png));
 
-    Button backgroundButton;
-    sf::Texture backTex;
     backTex.loadFromMemory(textures.OuterBox1png, sizeof(textures.OuterBox1png));
     backgroundButton.baseSprite.setTexture(backTex, true);
     backgroundButton.hoverSprite.setColor(sf::Color::Transparent);
@@ -37,27 +23,21 @@ int main()
     backgroundButton.baseColor = { 128, 128, 128, 255 };
     backgroundButton.hoverColor = { 128, 128, 128, 255 };
     backgroundButton.pressColor = { 128, 128, 128, 255 };
-    sf::Vector2i dragMousePos = {0,0};
-    bool bDraggingWindow = false;
     backgroundButton.OnPressBinding = [&]()
     {
         dragMousePos = sf::Mouse::getPosition(window);
         bDraggingWindow = true;
     };
 
-    sf::RectangleShape buttonsRect;
     buttonsRect.setFillColor(sf::Color(0, 0, 0, 255));
     buttonsRect.setPosition(8, 8);
     buttonsRect.setSize(sf::Vector2f(overlapTex.getSize().x * 3, overlapTex.getSize().y));
 
-    Button armButton;
-    sf::Texture armTex;
     armTex.loadFromMemory(textures.ArmButton1png, sizeof(textures.ArmButton1png));
     armButton.SetTextures(&armTex, &overlapTex);
     armButton.setPosition(8, 8);
     armButton.baseColor = { 180, 180, 180, 255 };
     armButton.hoverColor = { 30, 140, 30, 255 };
-    bool bArmed = false;
     armButton.OnReleaseBinding = [&]()
     {
         bArmed = !bArmed;
@@ -76,19 +56,13 @@ int main()
         armButton.UpdateColors();
     };
 
-    Button cpsButton;
-    sf::Texture cpsTex;
     cpsTex.loadFromMemory(textures.CPSButton0png, sizeof(textures.CPSButton0png));
-    sf::Texture cpsTex1;
     cpsTex1.loadFromMemory(textures.CPSButton1png, sizeof(textures.CPSButton1png));
-    sf::Texture cpsTex2;
     cpsTex2.loadFromMemory(textures.CPSButton2png, sizeof(textures.CPSButton2png));
     cpsButton.SetTextures(&cpsTex1, &overlapTex);
     cpsButton.setPosition(overlapTex.getSize().x + 8, 8);
     cpsButton.baseColor = { 180, 180, 180, 255 };
     cpsButton.hoverColor = { 140, 100, 30, 255 };
-    unsigned char cpsState = 1;
-    unsigned char cpsDowntime = 16;
     cpsButton.OnReleaseBinding = [&]()
     {
         if (cpsState < 2)
@@ -113,8 +87,6 @@ int main()
         }
     };
 
-    Button exitButton;
-    sf::Texture exitTex;
     exitTex.loadFromMemory(textures.ExitButton1png, sizeof(textures.ExitButton1png));
     exitButton.SetTextures(&exitTex, &overlapTex);
     exitButton.setPosition(overlapTex.getSize().x * 2 + 8, 8);
@@ -125,12 +97,55 @@ int main()
         exit(0);
     };
 
-    Button* buttons[] = {&backgroundButton, &armButton, &cpsButton, &exitButton};
+    buttons[0] = &backgroundButton;
+    buttons[1] = &armButton;
+    buttons[2] = &cpsButton;
+    buttons[3] = &exitButton;
 
     for (auto& e : buttons)
         e->FinalizeInit();
+}
 
-    //////////////////////////////////////////////////////////////////////////
+void CM3000v2::Update()
+{
+    for (auto& e : buttons)
+        e->Poll(window);
+
+    if (bDraggingWindow)
+    {
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        {
+            window.setPosition(sf::Mouse::getPosition() - dragMousePos);
+        }
+        else
+        {
+            bDraggingWindow = false;
+        }
+    }
+}
+
+void CM3000v2::Draw()
+{
+    buttons[0]->Draw(&window);
+    window.draw(buttonsRect);       //yo, sort this out
+    for (int i = 1; i < 4; i++)
+        buttons[i]->Draw(&window);
+}
+
+int CM3000v2::Run()
+{
+    window.create(sf::VideoMode(208, 80), "ClickMaster 3000", sf::Style::None);
+
+    MARGINS margins;
+    margins.cxLeftWidth = -1;
+    HWND hwnd = window.getSystemHandle();
+
+    SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+    DwmExtendFrameIntoClientArea(hwnd, &margins);
+
+    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);		//locks app in front of everything else
+
+    Init();
 
     sf::Clock clock;
     while (window.isOpen())
@@ -156,38 +171,20 @@ int main()
                 window.close();
         }
 
-        //////////////////////////////////////////////////////////////////////////
-
-        for(auto& e : buttons)
-            e->Poll(window);
-
-        if (bDraggingWindow)
-        {
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-            {
-                window.setPosition(sf::Mouse::getPosition() - dragMousePos);
-            }
-            else
-            {
-                bDraggingWindow = false;
-            }
-        }
-
-        //////////////////////////////////////////////////////////////////////////
+        Update();
 
         window.clear(sf::Color::Transparent);
 
-        //////////////////////////////////////////////////////////////////////////
-
-        buttons[0]->Draw(&window);
-        window.draw(buttonsRect);       //yo, sort this out
-        for (int i = 1; i < 4; i++)
-            buttons[i]->Draw(&window);
-
-        //////////////////////////////////////////////////////////////////////////
+        Draw();
 
         window.display();
     }
 
     return 0;
+}
+
+int main()
+{
+    CM3000v2 program;
+    return program.Run();
 }
---
+++ CM3000v2/Source/Source.h
@@ -0,0 +1,37 @@
+#pragma once
+
+class CM3000v2
+{
+    sf::RenderWindow window;
+
+    sf::Texture overlapTex;
+
+    Button backgroundButton;
+    sf::Texture backTex;
+    sf::Vector2i dragMousePos = { 0,0 };
+    bool bDraggingWindow = false;
+
+    sf::RectangleShape buttonsRect;
+
+    Button armButton;
+    sf::Texture armTex;
+    bool bArmed = false;
+
+    Button cpsButton;
+    sf::Texture cpsTex;
+    sf::Texture cpsTex1;
+    sf::Texture cpsTex2;
+    unsigned char cpsState = 1;
+    unsigned char cpsDowntime = 16;
+
+    Button exitButton;
+    sf::Texture exitTex;
+
+    Button* buttons[4];
+
+    void Init();
+    void Update();
+    void Draw();
+public:
+    int Run();
+};
